40d91d2b9fc081fc9bb9a5241e94479a90208f5e
Ronik <basnet.ronik622@gmail.com>
2024-11-11 13:57:11 +0545

    d

--- routes/cycleRoutes.js
+++ routes/cycleRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const { getAvailableCycles } = require('../controllers/cycleController');
 
-router.get('/available', authMiddleware, getAvailableCycles);
+router.get('/available', getAvailableCycles);
 
 module.exports = router;
