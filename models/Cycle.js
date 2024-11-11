f2309d72a60b472b65f33fcd9374cf4e9399f0c8
Ronik <basnet.ronik622@gmail.com>
2024-11-11 16:05:31 +0545

    ff

--- models/Cycle.js
+++ models/Cycle.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 
 const cycleSchema = new mongoose.Schema({
   cycle_type: { type: String, required: true },
+  name: { type: String},
+  color: { type: String},
   status: { type: String, enum: ['Available', 'Rented', 'Maintenance'], default: 'Available' },
   price_per_hour: { type: Number, required: true },
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  updated_at: { type: Date, default: Date.now },
+  image: { type: String}
 });
 
 module.exports = mongoose.model('Cycle', cycleSchema);
